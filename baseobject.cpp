f131032ad29fee02b3d37efb3b4f872e39ad3aea
luling-2001 <64293523+luling-2001@users.noreply.github.com>
2021-02-04 20:45:16 +0800

    Add files via upload

---
+++ baseobject.cpp
@@ -0,0 +1,6 @@
+#include "baseobject.h"
+
+baseObject::baseObject()
+{
+
+}
---
+++ baseobject.h
@@ -0,0 +1,16 @@
+#ifndef BASEOBJECT_H
+#define BASEOBJECT_H
+
+
+class baseObject
+{
+public:
+    baseObject();
+
+private:
+    double velocity;//移动速度
+    double lifecycle;//生命周期
+
+};
+
+#endif // BASEOBJECT_H

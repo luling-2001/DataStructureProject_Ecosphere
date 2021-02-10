a26525f950bca16d18a8b4d93527041fd9cb9da2
luling-2001 <64293523+luling-2001@users.noreply.github.com>
2021-02-10 11:44:10 +0800

    Update baseobject.cpp

--- baseobject.cpp
+++ baseobject.cpp
@@ -1,6 +1,26 @@
-#include "baseobject.h"  //试试
+#include "baseobject.h"
 
 baseObject::baseObject()
 {
 
+}
+
+baseObject::~baseObject(){
+
+}
+
+double baseObject::getVelocity(){
+    return velocity;
+}
+
+double baseObject::getLifecycle(){
+    return lifecycle;
+}
+
+void baseObject::setVelocity(double velocity){
+    this->velocity = velocity;
+}
+
+void baseObject::setLifecycle(double lifecycle){
+    this->lifecycle = lifecycle;
 }

d00b42bb649a20c3cf51509ec75b42cbb8f26816
luling-2001 <64293523+luling-2001@users.noreply.github.com>
2021-02-10 12:34:43 +0800

    补充了reset按钮的实现部分

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -21,3 +21,16 @@ void gamewindow::on_back_pushButton_clicked()
     w->show();
     this->close();
 }
+
+void gamewindow::on_reset_pushButton_clicked()
+{
+    //单击reset按钮，所有参数清零
+    grassesNumber = 0;
+    ui->grassesNumber_spinBox->setValue(0);
+
+    cowsNumber = 0;
+    ui->cowsNumber_spinBox->setValue(0);
+
+    tigersNumber = 0;
+    ui->tigersNumber_spinBox->setValue(0);
+}

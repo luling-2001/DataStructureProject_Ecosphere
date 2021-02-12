b4cfe032fa47e2ae162ebbb64620831ce8cadcfe
luling-2001 <64293523+luling-2001@users.noreply.github.com>
2021-02-12 22:04:12 +0800

    简单实现了一下move（），需改进

--- baseobject.cpp
+++ baseobject.cpp
@@ -1,8 +1,10 @@
 #include "baseobject.h"
+#include <QTime>
 
 baseObject::baseObject()
 {
-
+    //以当前系统时间创建随机数生成种子
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
 }
 
 baseObject::~baseObject(){
@@ -23,4 +25,21 @@ void baseObject::setVelocity(double velocity){
 
 void baseObject::setLifecycle(double lifecycle){
     this->lifecycle = lifecycle;
+}
+
+void baseObject::move(){
+    //获取当前位置
+    int pre_X = getX();
+    int pre_Y = getY();
+
+    //生成0-3的随机数
+    int i = qrand() % 4;
+    int new_X = pre_X + dir[i][0];
+    int new_Y = pre_Y + dir[i][1];
+
+    /////////////////////最后实现时要先判断碰撞情况后设置新位置///////////////////////
+
+    //设置新位置
+    setX(new_X);
+    setY(new_Y);
 }

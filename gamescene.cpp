e51c27bba9e6a2ffee85eade8384e76a9ad4c5ff
luling-2001 <64293523+luling-2001@users.noreply.github.com>
2021-02-13 17:39:35 +0800

    新增gameScene类，用于在gamewindow中呈现图片和动画

---
+++ gamescene.cpp
@@ -0,0 +1,9 @@
+#include "gamescene.h"
+
+gameScene::gameScene()
+{
+
+    ////////////////在初始化scene时，绘制scene的大小，颜色等/////////////////
+
+
+}
---
+++ gamescene.h
@@ -0,0 +1,13 @@
+#ifndef GAMESCENE_H
+#define GAMESCENE_H
+
+#include <QGraphicsScene>
+
+//gameScene类继承自QGraphicsScene类
+class gameScene : public QGraphicsScene
+{
+public:
+    gameScene();
+};
+
+#endif // GAMESCENE_H

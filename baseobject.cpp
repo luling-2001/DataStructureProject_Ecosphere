e3f68820b36db64689a9a09b8f2e387603389695
luling-2001 <64293523+luling-2001@users.noreply.github.com>
2021-04-15 09:01:34 +0800

    删除了move函数

--- baseobject.cpp
+++ baseobject.cpp
@@ -5,14 +5,11 @@ baseObject::baseObject()
 {
     //调用构造函数时的时间存储到start_time中
     start_time = QTime(0,0,0).secsTo(QTime::currentTime());
+
 }
 
 baseObject::~baseObject(){
 
-}
-
-double baseObject::getVelocity(){
-    return velocity;
 }
 
 double baseObject::getLifecycle(){
@@ -23,10 +20,6 @@ double baseObject::getEnergy(){
     return energy;
 }
 
-void baseObject::setVelocity(double velocity){
-    this->velocity = velocity;
-}
-
 void baseObject::setLifecycle(double lifecycle){
     this->lifecycle = lifecycle;
 }
@@ -35,24 +28,7 @@ void baseObject::setEnergy(double energy){
     this->energy = energy;
 }
 
-void baseObject::move(){
-    //获取当前位置
-    int pre_X = getX();
-    int pre_Y = getY();
 
-    //生成0-3的随机数
-    //以当前系统时间创建随机数生成种子
-    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
-    int i = qrand() % 4;
-    int new_X = pre_X + dir[i][0];
-    int new_Y = pre_Y + dir[i][1];
-
-    /////////////////////最后实现时要先判断碰撞情况后设置新位置///////////////////////
-
-    //设置新位置
-    setX(new_X);
-    setY(new_Y);
-}
 
 
 
@@ -78,3 +54,4 @@ void baseObject::die(){
     /////////////////////3.碰见上一级捕食者/////////////////////////
 
 }
+
